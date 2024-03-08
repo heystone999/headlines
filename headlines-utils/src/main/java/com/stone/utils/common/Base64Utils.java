c4a634bc88030425325ccacccdeac87d5c257941
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-03-08 00:03:22 -0500

    init prj

---
+++ headlines-utils/src/main/java/com/stone/utils/common/Base64Utils.java
@@ -0,0 +1,28 @@
+package com.stone.utils.common;
+
+
+import java.util.Base64;
+
+public class Base64Utils {
+
+    /**
+     * 解码
+     *
+     * @param base64
+     * @return
+     */
+    public static byte[] decode(String base64) {
+        return Base64.getDecoder().decode(base64);
+    }
+
+
+    /**
+     * 编码
+     *
+     * @param data
+     * @return
+     */
+    public static String encode(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+}

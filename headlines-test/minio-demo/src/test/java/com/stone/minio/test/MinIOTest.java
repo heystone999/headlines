72520e369ced8b3774aa836f4da9f7a864a83bee
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-03-16 13:49:56 -0400

    feat(app): Encapsulate MinIO as a Starter

--- headlines-test/minio-demo/src/test/java/com/stone/minio/test/MinIOTest.java
+++ headlines-test/minio-demo/src/test/java/com/stone/minio/test/MinIOTest.java
@@ -1,19 +1,37 @@
 package com.stone.minio.test;
 
+import com.stone.file.service.FileStorageService;
+import com.stone.minio.MinIOApplication;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+@SpringBootTest(classes = MinIOApplication.class)
+@RunWith(SpringRunner.class)
 public class MinIOTest {
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @Test
+    public void test() throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("../freemarker-demo/list.html");
+        String path = fileStorageService.uploadHtmlFile("", "list.html", fileInputStream);
+        System.out.println(path);
+    }
 
     /**
      * 把list.html文件上传到minio中，并且可以在浏览器中访问
      *
      * @param args
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         try {
             FileInputStream fileInputStream = new FileInputStream("headlines-test/freemarker-demo/list.html");
             MinioClient minioClient = MinioClient.builder().credentials("minioadmin", "minioadmin").endpoint("http://localhost:9000").build();
@@ -30,5 +48,5 @@ public class MinIOTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+    }*/
 }

0625bc7917d6f8fa9ca5459ee1e0deff43aac67a
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-06-10 18:29:50 -0400

    feat(schedule): Redis KEYS, SCAN, Pipeline

--- headlines-service/headlines-schedule/src/test/java/com/stone/schedule/test/RedisTest.java
+++ headlines-service/headlines-schedule/src/test/java/com/stone/schedule/test/RedisTest.java
@@ -1,13 +1,21 @@
 package com.stone.schedule.test;
 
+import com.alibaba.fastjson.JSON;
 import com.stone.common.redis.CacheService;
+import com.stone.model.schedule.dtos.Task;
 import com.stone.schedule.ScheduleApplication;
+import com.sun.istack.Nullable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 @SpringBootTest(classes = ScheduleApplication.class)
@@ -31,5 +39,47 @@ public class RedisTest {
 //        cacheService.zAdd("zset_key_001", "hello zset 004", 99999);
         Set<String> zset_key_001 = cacheService.zRangeByScore("zset_key_001", 0, 8888);
         System.out.println(zset_key_001);
+    }
+
+    @Test
+    public void testKeys() {
+//        Set<String> keys = cacheService.keys("future_*");
+//        System.out.println(keys);
+
+        Set<String> scan = cacheService.scan("future_*");
+        System.out.println(scan);
+    }
+
+    @Test
+    public void testPipe1() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            Task task = new Task();
+            task.setTaskType(1001);
+            task.setPriority(1);
+            task.setExecuteTime(new Date().getTime());
+            cacheService.lLeftPush("1001_1", JSON.toJSONString(task));
+        }
+        System.out.println("耗时" + (System.currentTimeMillis() - start));
+    }
+
+    @Test
+    public void testPipe2() {
+        long start = System.currentTimeMillis();
+        List<Object> objectList = cacheService.getstringRedisTemplate().executePipelined(new RedisCallback<Object>() {
+            @Nullable
+            @Override
+            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                for (int i = 0; i < 10000; i++) {
+                    Task task = new Task();
+                    task.setTaskType(1001);
+                    task.setPriority(1);
+                    task.setExecuteTime(new Date().getTime());
+                    redisConnection.lPush("1001_1".getBytes(), JSON.toJSONString(task).getBytes());
+                }
+                return null;
+            }
+        });
+        System.out.println("使用管道技术执行10000次自增操作共耗时:" + (System.currentTimeMillis() - start) + "毫秒");
     }
 }

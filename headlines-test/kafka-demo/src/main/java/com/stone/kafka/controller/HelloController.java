fd8c75b470fbc89b07bef04c2bcdd209f3530452
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-06-17 00:44:04 -0400

    feat(kafka): integrate Kafka with SpringBoot for sending and receiving messages

---
+++ headlines-test/kafka-demo/src/main/java/com/stone/kafka/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.stone.kafka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @GetMapping("/hello")
+    public String hello() {
+        kafkaTemplate.send("stone-topic", "STONE");
+        return "ok";
+    }
+}

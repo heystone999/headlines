d83af1159cfc2f15f30facc3ccf7e66dd2bde679
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-07-01 23:08:30 -0400

    feat(admin): admin-gateway

---
+++ headlines-gateway/headlines-admin-gateway/src/main/java/com/stone/admin/gateway/filter/AuthorizeFilter.java
@@ -0,0 +1,64 @@
+package com.stone.admin.gateway.filter;
+
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.stone.admin.gateway.util.AppJwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class AuthorizeFilter implements Ordered, GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+
+        if (request.getURI().getPath().contains("/login")) {
+            return chain.filter(exchange);
+        }
+
+        String token = request.getHeaders().getFirst("token");
+        if (StringUtils.isBlank(token)) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        try {
+            Claims claimsBody = AppJwtUtil.getClaimsBody(token);
+            int result = AppJwtUtil.verifyToken(claimsBody);
+            if (result == 1 || result == 2) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+
+            // 获取用户信息
+            Object userId = claimsBody.get("id");
+            // 存到header中
+            ServerHttpRequest serverHttpRequest = request.mutate().headers(httpHeaders -> {
+                httpHeaders.add("userId", userId + "");
+            }).build();
+            // 重置请求
+            exchange.mutate().request(serverHttpRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}

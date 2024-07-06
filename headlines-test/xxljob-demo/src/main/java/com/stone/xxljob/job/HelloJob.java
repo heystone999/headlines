aab4b44d90cf2d0cedae271f9f68846d161c2737
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-07-06 16:40:23 -0400

    feat(xxl-job): round-robin routing strategy

--- headlines-test/xxljob-demo/src/main/java/com/stone/xxljob/job/HelloJob.java
+++ headlines-test/xxljob-demo/src/main/java/com/stone/xxljob/job/HelloJob.java
@@ -1,12 +1,16 @@
 package com.stone.xxljob.job;
 
 import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloJob {
+    @Value("${server.port}")
+    private String port;
+
     @XxlJob("demoJobHandler")
     public void helloJob() {
-        System.out.println("执行xxl-job");
+        System.out.println("执行xxl-job" + port);
     }
 }

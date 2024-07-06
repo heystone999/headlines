01c9b3d1f641d0b125e91f964efee9e2ed57aaa9
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-07-06 16:56:10 -0400

    feat(xxl-job): sharding broadcast routing strategy

--- headlines-test/xxljob-demo/src/main/java/com/stone/xxljob/job/HelloJob.java
+++ headlines-test/xxljob-demo/src/main/java/com/stone/xxljob/job/HelloJob.java
@@ -1,8 +1,12 @@
 package com.stone.xxljob.job;
 
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class HelloJob {
@@ -12,5 +16,26 @@ public class HelloJob {
     @XxlJob("demoJobHandler")
     public void helloJob() {
         System.out.println("执行xxl-job" + port);
+    }
+
+    @XxlJob("shardingJobHandler")
+    public void shardingJobHandler() {
+        int shardIndex = XxlJobHelper.getShardIndex();
+        int shardTotal = XxlJobHelper.getShardTotal();
+
+        List<Integer> list = getList();
+        for (Integer integer : list) {
+            if (integer % shardTotal == shardIndex) {
+                System.out.println(shardIndex + "分片执行任务" + integer);
+            }
+        }
+    }
+
+    public List<Integer> getList() {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            list.add(i);
+        }
+        return list;
     }
 }
